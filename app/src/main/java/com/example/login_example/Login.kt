8011736b2ba10743016083235790a509fffbf357
franc <teofrenk99@gmail.com>
2021-03-30 15:54:17 +0200

    Prima versione con design login grigio

---
+++ app/src/main/java/com/example/login_example/Login.kt
@@ -0,0 +1,14 @@
+package com.example.login_example
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
